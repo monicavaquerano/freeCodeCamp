06bef0bd28e924a0a44cfc033c275b60acf6cc0a
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-09-02 19:30:19 +0200

    Create 38-word-blanks.js

---
+++ 2. JavaScript Algorithms and Data Structures/1. Basic JavaScript/38-word-blanks.js
@@ -0,0 +1,10 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "My " + myNoun + " is very " + myAdjective + " and " + myVerb + " " + myAdverb + "."; // Change this line
+// Only change code above this line
+
+console.log(wordBlanks)
