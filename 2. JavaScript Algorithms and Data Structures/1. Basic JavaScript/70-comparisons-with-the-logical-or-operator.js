0c94cf720e60a17e772a0c2e4e1cd6c3c86d3828
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-09-05 12:53:28 +0200

    Create 70-comparisons-with-the-logical-or-operator.js

---
+++ 2. JavaScript Algorithms and Data Structures/1. Basic JavaScript/70-comparisons-with-the-logical-or-operator.js
@@ -0,0 +1,14 @@
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+console.log(testLogicalOr(9));
+console.log(testLogicalOr(15));
+console.log(testLogicalOr(25));
