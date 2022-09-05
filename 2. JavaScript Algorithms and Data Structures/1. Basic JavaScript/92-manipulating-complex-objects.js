b6b74d99f516e09c810b280544a7e2ca21dcffc7
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-09-05 14:45:56 +0200

    Create 92-manipulating-complex-objects.js

---
+++ 2. JavaScript Algorithms and Data Structures/1. Basic JavaScript/92-manipulating-complex-objects.js
@@ -0,0 +1,23 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Rosalía",
+    "title": "Motomami",
+    "release_year": 2022,
+    "formats": [
+      "CD",
+      "MP3",
+      "LP"
+    ]
+  }
+];
