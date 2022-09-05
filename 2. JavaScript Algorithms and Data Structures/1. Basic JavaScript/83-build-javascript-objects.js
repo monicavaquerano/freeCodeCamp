7c9ecd76366ec30735df961e55b40dc52a4cd48e
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-09-05 14:37:01 +0200

    Create 83-build-javascript-objects.js

---
+++ 2. JavaScript Algorithms and Data Structures/1. Basic JavaScript/83-build-javascript-objects.js
@@ -0,0 +1,11 @@
+const myDog = {
+  // Only change code below this line
+"name": "Perla",
+"legs": 4,
+"tails": 1,
+"friends": ["Kaiser", "Rocco", "Kika", "Rex"]
+
+  // Only change code above this line
+};
+
+console.log(myDog.name + ' is beautiful´);
