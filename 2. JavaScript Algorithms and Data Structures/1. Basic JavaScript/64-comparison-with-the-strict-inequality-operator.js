6ed5b7678717dcc368c680debf11185ab3a8fa15
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-09-05 12:43:47 +0200

    Create 64-comparison-with-the-strict-inequality-operator.js

---
+++ 2. JavaScript Algorithms and Data Structures/1. Basic JavaScript/64-comparison-with-the-strict-inequality-operator.js
@@ -0,0 +1,10 @@
+// Setup
+function testStrictNotEqual(val) {
+  if (val !== 17) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+console.log(testStrictNotEqual(10));
+console.log(testStrictNotEqual(17));
