8915c686c3e8511e6ca679cbf3384ac6aca70fe4
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-09-15 13:32:14 +0200

    Create 23-match-whitespace.js

---
+++ 2. JavaScript Algorithms and Data Structures/3. Regular Expressions/23-match-whitespace.js
@@ -0,0 +1,7 @@
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // Change this line
+let result = sample.match(countWhiteSpace);
+
+console.log(result);
+
+/* You can search for whitespace using \s, which is a lowercase s. This pattern not only matches whitespace, but also carriage return, tab, form feed, and new line characters. You can think of it as similar to the character class [ \r\t\f\n\v]. */
