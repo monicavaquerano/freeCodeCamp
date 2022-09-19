4ef08a1589c34e5c48d534d302b3a299ca334035
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-09-19 12:57:46 +0200

    Create 7-catch-use-of-assignment-operator-instead-of-equality-operator.js

---
+++ 2. JavaScript Algorithms and Data Structures/4. Debugging/7-catch-use-of-assignment-operator-instead-of-equality-operator.js
@@ -0,0 +1,24 @@
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x == y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
+
+/* 
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x = y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result); */
