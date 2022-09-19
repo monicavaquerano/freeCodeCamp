e25479987bd8e41ceac2bcd212e71a7c6f194683
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-09-19 12:08:23 +0200

    Create 5-catch-unclosed-parentheses-brackets-braces-and-quotes.js

---
+++ 2. JavaScript Algorithms and Data Structures/4. Debugging/5-catch-unclosed-parentheses-brackets-braces-and-quotes.js
@@ -0,0 +1,8 @@
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
+
+
+/* let myArray = [1, 2, 3;
+let arraySum = myArray.reduce((previous, current =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`); */
