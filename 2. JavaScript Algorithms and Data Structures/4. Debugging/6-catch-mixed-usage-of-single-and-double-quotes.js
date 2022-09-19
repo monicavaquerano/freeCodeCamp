7cddb4cf36de30bd2094ce59bdf3caf5587e24e8
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-09-19 12:55:36 +0200

    Create 6-catch-mixed-usage-of-single-and-double-quotes.js

---
+++ 2. JavaScript Algorithms and Data Structures/4. Debugging/6-catch-mixed-usage-of-single-and-double-quotes.js
@@ -0,0 +1,6 @@
+let innerHtml = "<p>Click here to <a href=\"#Home\">return home</a></p>";
+console.log(innerHtml);
+
+
+/* let innerHtml = "<p>Click here to <a href="#Home">return home</a></p>";
+console.log(innerHtml); */
