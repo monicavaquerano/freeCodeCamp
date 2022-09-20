30d8285b64063879d24c5ee78462aefd3bb097c6
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-09-20 17:47:01 +0200

    Create 12-prevent-infinite-loops-with-a-valid-terminal-condition.js

---
+++ 2. JavaScript Algorithms and Data Structures/4. Debugging/12-prevent-infinite-loops-with-a-valid-terminal-condition.js
@@ -0,0 +1,13 @@
+//Funcion con fin
+function myFunc() {
+  for (let i = 1; i <= 4; i += 2) {
+    console.log("Still going!");
+  }
+}
+
+//Funcion que correrá para siempre
+/* function myFunc() {
+  for (let i = 1; i != 4; i += 2) {
+    console.log("Still going!");
+  }
+} */
