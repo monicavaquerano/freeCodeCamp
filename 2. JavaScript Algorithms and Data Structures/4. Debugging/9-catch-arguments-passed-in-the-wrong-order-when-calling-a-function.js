65a205bd38a1be19215b7c1e4cb24d41788970d6
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-09-20 17:44:36 +0200

    Create 9-catch-arguments-passed-in-the-wrong-order-when-calling-a-function.js

---
+++ 2. JavaScript Algorithms and Data Structures/4. Debugging/9-catch-arguments-passed-in-the-wrong-order-when-calling-a-function.js
@@ -0,0 +1,19 @@
+function raiseToPower(b, e) {
+  return Math.pow(b, e);
+}
+
+let base = 2;
+let exp = 3;
+let power = raiseToPower(base, exp);
+console.log(power);
+
+
+
+/* function raiseToPower(b, e) {
+  return Math.pow(b, e);
+}
+
+let base = 2;
+let exp = 3;
+let power = raiseToPower(exp, base);
+console.log(power); */
