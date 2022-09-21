04173482f42131c071d889c10f73fb42c4e1ab9b
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-09-21 13:22:55 +0200

    Create 5-remove-items-using-splice.js

---
+++ 2. JavaScript Algorithms and Data Structures/5. Basic Data Structures/5-remove-items-using-splice.js
@@ -0,0 +1,16 @@
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(0,2);
+arr.splice(1,1);
+arr.splice(1,1);
+arr.splice(2,2);
+// Only change code above this line
+console.log(arr);
+
+// or
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(1,4);
+// Only change code above this line
+console.log(arr);
