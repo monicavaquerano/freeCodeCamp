fcef8581fb4427d21b8e18ffb5643581aa51747a
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-10-14 12:47:10 +0200

    Create 20-reset-an-inherited-constructor-property.js

---
+++ 2. JavaScript Algorithms and Data Structures/7. Object Oriented Programming/20-reset-an-inherited-constructor-property.js
@@ -0,0 +1,23 @@
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+
+let duck = new Bird();
+let beagle = new Dog();
+
+function cons(){
+  if (duck.constructor === Bird){
+  return true;
+} else { 
+  return false;
+  }
+}
+console.log(cons())
