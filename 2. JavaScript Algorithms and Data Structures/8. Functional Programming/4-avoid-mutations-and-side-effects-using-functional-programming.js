80499d87fb22ebc260c0df8b22acfa10a96bfb15
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-10-25 14:40:41 +0200

    Create 4-avoid-mutations-and-side-effects-using-functional-programming.js

---
+++ 2. JavaScript Algorithms and Data Structures/8. Functional Programming/4-avoid-mutations-and-side-effects-using-functional-programming.js
@@ -0,0 +1,11 @@
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+let incremented;
+incremented = fixedValue + 1;
+return incremented;
+/* or return fixedValue + 1; */
+  // Only change code above this line
+}
