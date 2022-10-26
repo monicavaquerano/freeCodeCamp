fbce5e87532022e41a053821f51d0415e507c9e6
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2022-10-26 14:22:31 +0200

    Create 13-combine-two-arrays-using-the-concat-method.js

---
+++ 2. JavaScript Algorithms and Data Structures/8. Functional Programming/13-combine-two-arrays-using-the-concat-method.js
@@ -0,0 +1,12 @@
+function nonMutatingConcat(original, attach) {
+  // Only change code below this line
+
+let newArr = original.concat(attach);
+return newArr;
+
+  // Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+console.log(nonMutatingConcat(first, second));
