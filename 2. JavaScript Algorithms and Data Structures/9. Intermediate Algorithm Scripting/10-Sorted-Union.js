10c3ec1bcd1fcf331bf12fffcd7006dd0d7c619d
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2023-08-01 19:34:49 +0200

    Update 10-Sorted-Union.js

--- 2. JavaScript Algorithms and Data Structures/9. Intermediate Algorithm Scripting/10-Sorted-Union.js
+++ 2. JavaScript Algorithms and Data Structures/9. Intermediate Algorithm Scripting/10-Sorted-Union.js
@@ -1,19 +1,14 @@
-// FALTA TERMINAR ESTE EJERCICIO
-
-function uniteUnique(arr) {
-  let unique = [];
-  for (let i = 0; i < arr.length; i++)
-  {
-    for (let j = 0; j < arr.length; j++)
-    {
-      if (arr[i] != arr[j])
-      {
-        unique.push(arr[j]);
+function uniteUnique(...arr) {
+  let args = arr;
+  let unique = []
+  for (let i = 0; i < args.length; i++) {
+    for (let j = 0; j < args[i].length; j++) {
+      if (!unique.includes(args[i][j])) {
+        unique.push(args[i][j]);
       }
     }
-    return unique;
   }
-  
+    return unique;
 }
 
 console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])); // [1, 3, 2, 5, 4]
