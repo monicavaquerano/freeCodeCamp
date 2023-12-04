fdc4b88bafe08128e6317cbde84f449f4d64fc34
Monica Vaquerano <106926593+monicavaquerano@users.noreply.github.com>
2023-12-04 16:21:04 +0100

    Create 15_drop_it.js

---
+++ 2. JavaScript Algorithms and Data Structures/9. Intermediate Algorithm Scripting/15_drop_it.js
@@ -0,0 +1,11 @@
+function dropElements(arr, func) {
+  
+  while (arr.length > 0 && !func(arr[0])) {
+    arr.shift();
+  }
+  return arr;
+
+}
+
+console.log(dropElements([1, 2, 3], function(n) {return n < 3; }));
+console.log(dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}));
